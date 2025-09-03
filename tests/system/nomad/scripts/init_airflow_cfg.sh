a49b82f5bd0ed6de315a5883eabc210ad3e927d6
Judit Novak <judit.novak@gmail.com>
2025-09-03 22:13:47 +0200

    [TESTS] Config creaating script wouldn't overwrite

--- tests/system/nomad/scripts/init_airflow_cfg.sh
+++ tests/system/nomad/scripts/init_airflow_cfg.sh
@@ -4,10 +4,18 @@ if [[ -z "$AIRFLOW_HOME" ]]; then
     echo "AIRFLOW_HOME must be defined"
 fi
 
+AIRFLOW_CFG=$AIRFLOW_HOME/airflow.cfg
+
+if [ -f "$AIRFLOW_CFG" ]; then
+    echo "[ERROR] $AIRFLOW_CFG is an existing file, exiting..."
+    exit 1
+fi
 
 # Due to occasional CI errors on the file missing
 touch $AIRFLOW_HOME/airflow.cfg
 
 cat tests/system/nomad/config/unit_tests.cfg.template | \
     sed "s|<SYSTEST_ROOT>|$PWD/tests/system/nomad|g"  | \
-    sed "s|<SYSTEST_AIRFLOW_HOME>|$AIRFLOW_HOME|g" > $AIRFLOW_HOME/airflow.cfg
+    sed "s|<SYSTEST_AIRFLOW_HOME>|$AIRFLOW_HOME|g" > $AIRFLOW_CFG
+
+echo "$AIRFLOW_CFG was generated successfully"
