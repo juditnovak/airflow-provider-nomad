800da09c419450c621c506e93f5cc0c3dcfe47f2
Judit Novak <judit.novak@gmail.com>
2025-09-04 11:17:35 +0200

    [TESTS] Environment fully prepared to integrated to enable secure testing

--- tests/system/nomad/runner/scripts/create_dynamic_logs_volume.sh
+++ tests/system/nomad/runner/scripts/create_dynamic_logs_volume.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
-INFILE=$1
 
-if [[ -z "$INFILE" ]]; then
-    echo "Usage: $0 <json-file>"
-    exit 1
-fi
+help () {
+    echo "Usage: $0 [-h] [-s] <json-file>"
+    echo
+    echo" -h    Display this message"
+    echo" -s    Secure (use test certificates)"
+}
 
+
+# A POSIX variable
+OPTIND=1                    # Reset in case getopts has been used previously in the shell.
+
+# Initializing variables
 PROTOCOL=http
 HOST=localhost
 PORT=4646
+
+while getopts "sh:" opt; do
+  case "$opt" in
+    h)
+      help
+      exit 0
+      ;;
+    s)  PROTOCOL='https'
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+[ "${1:-}" = "--" ] && shift
+
+
+INFILE=$1
+if [[ -z "$INFILE" ]]; then
+    help
+    exit 1
+fi
+
+
+CURL_PARAM=""
+if [ $PROTOCOL == "https" ]
+then
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    CERTDIR="${SCRIPT_DIR%/*}/certs"
+    CURL_PARAM="--cert $CERTDIR/global-cli-nomad.pem --key $CERTDIR/global-cli-nomad-key.pem --cacert $CERTDIR/nomad-agent-ca.pem"
+fi
+
+
 URL=${PROTOCOL}://${HOST}:${PORT}
 
-
 printf "Creating dynamic host volume on ${HOST}\n"
-ID=$(curl -s --request PUT --data @${INFILE} ${URL}/v1/volume/host/create \
+ID=$(curl $CURL_PARAM -s --request PUT --data @${INFILE} ${URL}/v1/volume/host/create \
 	| jq  2> /dev/null \
 	| grep '"ID":' \
 	| cut -d':' -f2 \
@@ -28,7 +65,7 @@ fi
 printf "Created dynamic host volume ${ID}\n"
 
 # The volume has to be rw for runner
-HOSTPATH=$(curl -s --request GET ${URL}/v1/volume/host/$ID \
+HOSTPATH=$(curl $CURL_PARAM -s --request GET ${URL}/v1/volume/host/$ID \
 	| jq  2> /dev/null \
 	| grep HostPath \
 	| cut -d':' -f2 \
