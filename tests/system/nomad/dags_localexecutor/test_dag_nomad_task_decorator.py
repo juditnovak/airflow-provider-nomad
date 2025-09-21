c22fbb83c7651f3df5ce0cd2d2b130d0c083cfcc
Judit Novak <judit.novak@gmail.com>
2025-09-21 20:07:52 +0200

    [TEST] Missing system tests added

---
+++ tests/system/nomad/dags_localexecutor/test_dag_nomad_task_decorator.py
@@ -0,0 +1,105 @@
+import datetime
+import os
+
+import attrs
+import pendulum
+from airflow.sdk import DAG
+
+from airflow.providers.nomad.decorators.task import nomad_task
+
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+
+DAG_ID = "test_nomad_task_decorator_dag"
+JOB_NAME = "task-test-config-default-job-template-hcl"
+JOB_NAMESPACE = "default"
+
+
+##############################################################################
+# DAG with relative location
+#
+# Airflow SDK DAGs are constucted with their absolute path ('fileloc' attr)
+# instead of the relative location within the 'dags-folder'.
+# In order to allow for remote job submission, this requires a patch
+##############################################################################
+
+
+def _default_fileloc() -> str:
+    return os.path.basename(__file__)
+
+
+def _all_after_dag_id_to_kw_only(cls, fields: list[attrs.Attribute]):
+    i = iter(fields)
+    f = next(i)
+    if f.name != "dag_id":
+        raise RuntimeError("dag_id was not the first field")
+    yield f
+
+    for f in i:
+        yield f.evolve(kw_only=True)
+
+
+@attrs.define(repr=False, field_transformer=_all_after_dag_id_to_kw_only, slots=False)  # pyright: ignore[reportArgumentType]
+class myDAG(DAG):
+    fileloc: str = attrs.field(init=False, factory=_default_fileloc)
+
+    def __hash__(self):
+        return super().__hash__()
+
+
+##############################################################################
+
+
+content = """
+job "nomad-test-hcl" {
+  type = "batch"
+
+  group "example" {
+    count = 1
+    task "uptime" {
+      driver = "docker"
+      config {
+        image = "alpine:latest"
+        args = ["uptime"]
+      }
+    }
+  }
+}
+""".strip()
+
+
+with myDAG(
+    dag_id=DAG_ID,
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["nomad", "nomadjoboperator", "nomadexecutor"],
+) as dag:
+    # def test_nomad_task_decorator_dag():
+
+    @nomad_task(template_content=content, image="alpine:3.21")
+    def nomad_command_date():
+        return ["date"]
+
+    @nomad_task()
+    def nomad_command_uptime():
+        return ["uptime"]
+
+    nomad_command_uptime() >> nomad_command_date()  # type: ignore [reportUnusedExpression]
+
+
+# # Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+try:
+    from airflow.configuration import conf
+    from tests_common.test_utils.system_tests import get_test_run  # noqa: E402
+
+    from ..constants import TEST_DAGS_LOCALEXECUTOR_PATH
+
+    os.environ["TEST_DAGS_PATH"] = str(TEST_DAGS_LOCALEXECUTOR_PATH)
+
+    # Sadly none of the DAG executor settings are considered in the test environment
+    # Running it only in a pre-configured environment
+    if conf.get("core", "executor") == "LocalExecutor":
+        test_run = get_test_run(dag)
+except ImportError:
+    pass
