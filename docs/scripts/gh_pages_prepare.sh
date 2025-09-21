74656f782f0b3308a5dbacd1090b69a88a8ef6f0
Judit Novak <judit.novak@gmail.com>
2025-09-21 14:50:48 +0200

    [CI] pre-run script

---
+++ docs/scripts/gh_pages_prepare.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# NOTE!
+# Airflow normally stores these docs under /docs/. A number of references are hardcoded to this location
+# or may expect http(s)://<host>/docs/ structure (with no further references in-between)
+#
+# The hacks below ensure docs availability from GH pages published sources
+
+grep -r '"/external/' docs | cut -d: -f1 | uniq | xargs sed -i 's!"/external/!"/airflow-provider-nomad/external/!g'
+grep -r '"/_gen/' docs | cut -d: -f1 | uniq | xargs sed -i 's!"/_gen/!"/airflow-provider-nomad/_gen/!g'
+grep -r  '"/docs/"' ./ | cut -d: -f1 | uniq | xargs sed -i 's!"/docs/"!"/airflow-provider-nomad/"!g'
