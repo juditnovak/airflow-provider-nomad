0b681483ecf941bd12a3bfe565c5ec6455b68c33
Judit Novak <judit.novak@gmail.com>
2025-09-21 19:40:03 +0200

    [TESTS] Unit and System tests

--- tests/system/nomad/test_nomad_manager.py
+++ tests/system/nomad/test_nomad_manager.py
@@ -4,12 +4,13 @@
 ###############################################################################
 
 import json
+import logging
 
 from airflow.providers.nomad.manager import NomadManager
 from airflow.providers.nomad.models import NomadJobModel
 
 
-def test_nomad_job_operator_parse_template_hcl(test_datadir):
+def test_nomad_mgr_parse_template_hcl(test_datadir):
     mgr = NomadManager()
     mgr.initialize()
 
@@ -24,3 +25,38 @@ def test_nomad_job_operator_parse_template_hcl(test_datadir):
     assert NomadJobModel.model_validate_json(json.dumps(dict_content)) == mgr.parse_template_hcl(
         hcl_content
     )
+
+
+def test_nomad_mgr_retry_register_job(test_datadir, caplog):
+    mgr = NomadManager()
+    mgr.initialize()
+
+    with open(test_datadir / "simple_batch.hcl") as tpl_file:
+        template = mgr.parse_template_content(tpl_file.read())
+
+    assert template, "Error retrieving template"
+    # Ruining the template
+    template.Job.ID = " "
+
+    with caplog.at_level(logging.ERROR):
+        error = mgr.register_job(template)
+
+    assert (
+        error
+        == "The BaseNomadException was raised with following response: 1 error occurred:\n\t* Job ID contains a space\n\n."
+    )
+    assert caplog.text.count("Job ID contains a space") == 3
+
+
+def test_nomad_mgr_retry_deregister_job(caplog):
+    mgr = NomadManager()
+    mgr.initialize()
+
+    with caplog.at_level(logging.ERROR):
+        error = mgr.deregister_job("")
+
+    assert (
+        error
+        == "The BaseNomadException was raised with following response: missing job ID for deregistering."
+    )
+    assert caplog.text.count("missing job ID for deregistering") == 3
