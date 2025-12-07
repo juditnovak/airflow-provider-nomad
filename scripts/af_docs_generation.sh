aa64f826bd15414befacfbe39d66e12276383d63
Judit Novak <judit.novak@gmail.com>
2025-12-07 22:57:29 +0100

    [minor_adm_fixes]: [DOCS] Generation script cosmetics

--- scripts/af_docs_generation.sh
+++ scripts/af_docs_generation.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cur_dir=$(basename $CWD)
+cur_dir=$(basename $PWD)
 
 if [[ $cur_dir == "nomad_provider" ]]
 then
@@ -9,11 +9,14 @@ then
 fi
 
 cp -r ../nomad_provider/* providers/nomad/ 
-rm -fr providers/nomad/docs/_api; rm -fr providers/nomad/docs
+rm -fr providers/nomad/docs/_api
+rm -fr providers/nomad/docs
 cp -r ../nomad_provider/docs-rst/ ./providers/nomad/docs/
 uv run  --group docs build-docs --package-filter apache-airflow-providers-nomad nomad
 
-# Fixing navigation bar links
 
-grep -r '<a href="/">' docs | cut -d: -f1 | uniq | xargs sed -i 's!<a href="/">!<a href="https://airflow.apache.org/">!g'
-grep -r '<a class="navbar__text-link" href="/' docs | cut -d: -f1 | uniq | xargs sed -i 's!<a class="navbar__text-link" href="/!<a class="navbar__text-link" href="https://airflow.apache.org/!g'
+# Fixing navigation bar links
+# Done on the pipeline, commented here
+#
+# grep -r '<a href="/">' docs | cut -d: -f1 | uniq | xargs sed -i 's!<a href="/">!<a href="https://airflow.apache.org/">!g'
+# grep -r '<a class="navbar__text-link" href="/' docs | cut -d: -f1 | uniq | xargs sed -i 's!<a class="navbar__text-link" href="/!<a class="navbar__text-link" href="https://airflow.apache.org/!g'
