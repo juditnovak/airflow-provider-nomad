ae7a98ab7344d152045d3bb221557f1ccf0c8318
Judit Novak <judit.novak@gmail.com>
2025-12-10 14:13:12 +0100

    [CI] Updating pipelines with LocalExecutor dedicated config

--- tests/system/nomad/scripts/init_airflow_cfg.sh
+++ tests/system/nomad/scripts/init_airflow_cfg.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+TPL_FILE=unit_tests.cfg.template
+if [[ $1 ]]
+then
+    TPL_FILE=unit_tests_$1.cfg.template
+fi
+
+TPL_PATH=tests/system/nomad/config/$TPL_FILE
+
+if [[ ! -f $TPL_PATH ]]
+then
+    echo "$TPL_PATH doesn't exist, exiting"
+    exit 2
+fi
+
 if [[ -z "$AIRFLOW_HOME" ]]; then
     echo "AIRFLOW_HOME must be defined"
 fi
@@ -12,9 +26,9 @@ if [ -f "$AIRFLOW_CFG" ]; then
 fi
 
 # Due to occasional CI errors on the file missing
-touch $AIRFLOW_HOME/airflow.cfg
+touch $AIRFLOW_CFG
 
-cat tests/system/nomad/config/unit_tests.cfg.template | \
+cat $TPL_PATH | \
     sed "s|<SYSTEST_ROOT>|$PWD/tests/system/nomad|g"  | \
     sed "s|<SYSTEST_AIRFLOW_HOME>|$AIRFLOW_HOME|g" > $AIRFLOW_CFG
 
